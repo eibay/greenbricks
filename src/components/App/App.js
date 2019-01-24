86fff10cd471593091855131a9b4306f487d9139
Elmer Ibayan <eibayan@gmail.com>
2019-01-24 19:53:35 +1100

    init files

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component{
+    render() {
+        console.log(this.props)
+        return(
+            <h1>
+                {}
+            </h1>
+        )
+    }
+
+}
+
+export default App;
