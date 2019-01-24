2ff5735cd9a639d0e6f8a0a7ff197a66698bea10
Elmer Ibayan <eibayan@gmail.com>
2019-01-25 07:41:01 +1100

    add card styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 import HouseContainer from "../Houses/HouseContainer";
+import { Row, Column } from '../Grid/Grid';
+import '../Grid/Grid.css';
+import './App.css';
+
 
 class App extends React.Component{
     render() {
         return(
-            <div>
-                <HouseContainer />
-            </div>
+            <main className="app">
+                <Row>
+                    <Column size={3}> 
+                        <HouseContainer />
+                    </Column>
+                </Row>
+            </main>
         )
     }
-
 }
 
 export default App;
--- src/components/Houses/House.js
+++ src/components/Houses/House.js
@@ -1,28 +1,33 @@
 import React from 'react';
 
 import Panel from '../Panel/Panel';
+import './House.css';
 
 class House extends React.Component {
 
     render(){
         const houses = (this.props.houses.map(house => {
+            const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(house.listPrice)
             return (
-                <div key={house.listingId}>
-                    <h1>{house.listPrice}</h1>
-                    <img src={`${house.images[0].small}`} alt="medium" />
-                    <div>{house.listingStatus}</div>
-                    <div>{house.listingType}</div>
-                    <div>{house.lotArea} Sq.ft</div>
-                    <div>{house.bedroomsTotal} Beds</div>
-                    <div>{house.bathroomsTotal} Baths</div>
-                    <div>
-                        {house.streetNumber}
-                        {house.streetName}
-                        {house.city}
-                        {house.state}
-                        {house.postcode}
+                <div key={house.listingId} className="card-house">
+                    <div className="card-house__inner">
+                    <img src={`${house.images[0].small}`} alt="medium" width="100%"/>
+                    <div className="card-house__body">
+                        <div className="listing-status"><p>{house.listingStatus}</p></div>
+                        <h2>{price}</h2>
+                        <div>
+                            {`${house.streetNumber} ${house.streetName}`}
+                        </div>
+                        <div>
+                            {`${house.city} ${house.state} ${house.postcode}`}
+                        </div>
+                        <br />
+                        <hr></hr>
+                        <br />
+                        <div>{`${house.bedroomsTotal} Beds  | ${house.bathroomsTotal} Baths  |  ${house.lotArea} Sq.ft `}</div>
+                        <div>{house.listingType}</div>
                     </div>
-                    <p>{house.description}</p>
+                    </div>
                 </div>
             )
         }))
