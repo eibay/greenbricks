f70dd2b95bde4f82ec353f821cf9149bfd2306ed
Elmer Ibayan <eibayan@gmail.com>
2019-01-25 05:14:37 +1100

    add panel and grid layout styles

--- src/components/Houses/House.js
+++ src/components/Houses/House.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+import Panel from '../Panel/Panel';
 
 class House extends React.Component {
 
-
     render(){
-        return(
-            <div>
-                {this.props.houses.map(house => {
-                    return <div key={house.listingId}>
-                        <h1>{house.listingId}</h1>
+        const houses = (this.props.houses.map(house => {
+            return (
+                <div key={house.listingId}>
+                    <h1>{house.listPrice}</h1>
+                    <img src={`${house.images[0].small}`} alt="medium" />
+                    <div>{house.listingStatus}</div>
+                    <div>{house.listingType}</div>
+                    <div>{house.lotArea} Sq.ft</div>
+                    <div>{house.bedroomsTotal} Beds</div>
+                    <div>{house.bathroomsTotal} Baths</div>
+                    <div>
+                        {house.streetNumber}
+                        {house.streetName}
+                        {house.city}
+                        {house.state}
+                        {house.postcode}
                     </div>
-                })}
-            </div>
-        )}
+                    <p>{house.description}</p>
+                </div>
+            )
+        }))
+
+        return(
+            <Panel title="Current Listing">
+                {houses}
+            </Panel>
+        )
+    }
 }
 
 export default House;
